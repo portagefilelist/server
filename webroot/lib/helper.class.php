22b0692cef20602515cfe243270dc9c8812cb50f
Banana <mail@bananas-playground.net>
2025-02-15 00:38:44 +0100

    another cleanup
    Signed-off-by: Banana <mail@bananas-playground.net>

--- webroot/lib/helper.class.php
+++ webroot/lib/helper.class.php
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see http://www.gnu.org/licenses/gpl-3.0.
  *
- * pre 2023 - https://github.com/tuxmainy
+ * pre 2023 https://github.com/tuxmainy
  * 2023 https://www.bananas-playground.net/projekt/portagefilelist/
  */
 
